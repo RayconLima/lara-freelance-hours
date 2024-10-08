1e62a85ba4cd900888faf8ed071b2f8e44992ff3
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-10-08 10:38:59 -0400

    feat: :sparkles: add ProjectController in web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', WelcomeController::class);
+
+
+Route::get('/', [ProjectController::class, 'index'])->name('projects.index');
+Route::get('/project/{project}', [ProjectController::class, 'show'])->name('projects.show');
