45b4fe51d8f9e4ab3d8b7067dbe01ed4f60fb8a8
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-10-08 09:44:03 -0400

    feat: :sparkles: add project resource

---
+++ app/Models/Project.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Project extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProjectFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'ends_at',
+        'status',
+        'tech_stack',
+        'created_by',
+    ];
+
+    public function casts() {
+        return [
+            'tech_stack' => 'array',
+        ];
+    }
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
