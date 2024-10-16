47a56c284259d67244c12febae25d56268b69777
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-10-16 17:06:18 -0400

    feat(site): :bug: return technologies by Project

--- resources/views/components/projects/simple-card.blade.php
+++ resources/views/components/projects/simple-card.blade.php
@@ -28,9 +28,8 @@
     <div class="py-4 space-y-4">
         <div class="uppercase font-bold text-[#8C8C9A] text-[12px]">Tecnologias</div>
         <div class="flex gap-[8px] items-center pb-2">
-            {{-- {{ $project->tech_stack }} --}}
-            @foreach($project->tech_stack as $tech)
-                <x-ui.tech :icon="$tech" :text="$tech"/>
+            @foreach($project->technologies as $tech)
+                <x-ui.tech :icon="$tech->key" :text="$tech->key"/>
             @endforeach
         </div>
     </div>
