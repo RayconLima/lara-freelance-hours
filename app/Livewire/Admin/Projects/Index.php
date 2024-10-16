0d66f30d94d10c1b601f3fc8349e8a926940c79b
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-10-16 16:58:42 -0400

    feat(admin): :sparkles: add search field to filter project

--- app/Livewire/Admin/Projects/Index.php
+++ app/Livewire/Admin/Projects/Index.php
@@ -3,24 +3,32 @@
 namespace App\Livewire\Admin\Projects;
 
 use App\Models\Project;
-use Livewire\Attributes\On;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Url;
 
 class Index extends Component
 {
+    #[Url] 
     public string $search = '';
 
-    #[On('project::created')]
+    #[On('project::created', 'project::saved')]
     public function render()
     {
-        return view('livewire.admin.projects.index', [
-            'projects' => Project::where('title', 'LIKE', "%{$this->search}%")->get(),
-        ]);
+        return view('livewire.admin.projects.index');
+    }
+
+    #[Computed()]
+    public function projects()
+    {
+        return  Project::search($this->search)->get();
     }
     
     public function destroy(Project $project)
     {
         $project->delete();
+        $this->dispatch('project::saved');
         return redirect()->route('admin.projects.index');
     }
 }
--- resources/views/livewire/admin/projects/index.blade.php
+++ resources/views/livewire/admin/projects/index.blade.php
@@ -1,5 +1,8 @@
 <div>
-<livewire:admin.projects.create />
+  <div class="flex justify-between mb-2">
+    <input type="text" wire:model.live="search">
+    <livewire:admin.projects.create />
+  </div>
 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
   <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
     <tr>
@@ -22,10 +25,9 @@
     </tr>
   </thead>
   <tbody>
-    @foreach($projects as $project)
-      <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+    @foreach($this->projects as $project)
+      <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" wire:key="{{ $project->id }}">
         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">  
-
           {{ $project->title }}
         </th>
         <td class="px-6 py-4">
