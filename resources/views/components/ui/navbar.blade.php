601aba8d437ac16ae813c17276dbf0f04513ac4f
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-10-17 05:53:44 -0400

    refactor: :fire: remove signout tag link by navbar ui component

--- resources/views/components/ui/navbar.blade.php
+++ resources/views/components/ui/navbar.blade.php
@@ -45,7 +45,6 @@
                     {{ __('Log Out') }}
                 </x-dropdown-link>
             </form>
-            {{-- <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a> --}}
           </li>
         </ul>
       </div>
@@ -71,36 +70,3 @@
     </div>
   </div>
 </nav>
-
-{{-- todo: remover classe não responsiva --}}
-{{-- <nav class="border-[#1E1E2C] border-2 w-full py-[18px] ">
-    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-        <a class="text-white font-bold flex items-center gap-2">
-            <x-ui.logo class="w-10"/>
-            <span class="text-xl">FreelanceHours</span>
-        </a>
-
-        <!-- Menu Desktop -->
-        <ul class="hidden md:flex text-[#C3C3D1] items-center gap-4 text-[16px]">
-            <li class="hover:underline cursor-pointer"><a>Anunciar um projeto</a></li>
-            <li class="hover:underline cursor-pointer"><a href="{{ route('projects.index') }}">Procurar um projeto</a></li>
-            <li class="hover:underline cursor-pointer"><a>Como funciona?</a></li>
-        </ul>
-
-        <!-- Ícones e Menu Hamburguer -->
-        <div class="flex items-center gap-4">
-            <x-ui.icons.profile class="w-8 h-8"/>
-            <!-- Ícone de menu hamburguer visível em dispositivos móveis -->
-            <x-ui.icons.burguer class="w-8 h-8 md:hidden"/>
-        </div>
-    </div>
-
-    <!-- Menu Mobile -->
-    <div class="md:hidden px-4 mt-2">
-        <ul class="flex flex-col text-[#C3C3D1] text-[16px] gap-2">
-            <li class="hover:underline cursor-pointer"><a>Anunciar um projeto</a></li>
-            <li class="hover:underline cursor-pointer"><a href="{{ route('projects.index') }}">Procurar um projeto</a></li>
-            <li class="hover:underline cursor-pointer"><a>Como funciona?</a></li>
-        </ul>
-    </div>
-</nav> --}}
